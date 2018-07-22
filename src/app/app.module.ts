c65062f7c451fa25bf340bbddd6e020158817262
Alexey Eremin <a.eremin.msu@gmail.com>
2018-07-23 00:09:25 +0200

    Routing skeleton, empty overview page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from 'src/app/core/app.routing.module';
 import { AppComponent } from './app.component';
+import { OverviewPageComponent } from './overview-page/overview-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    OverviewPageComponent,
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/core/app.routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { OverviewPageComponent } from '../overview-page/overview-page.component';
+
+const routes: Routes = [
+  {path : '', component : OverviewPageComponent},
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes),
+  ],
+  exports: [
+    RouterModule,
+  ],
+  declarations: [],
+})
+export class AppRoutingModule { }
