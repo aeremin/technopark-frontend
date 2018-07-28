fce5cc8d8f2653898530559237fdc0421a9531fa
Alexey Eremin <a.eremin.msu@gmail.com>
2018-07-28 14:36:19 +0200

    Better variable name

--- src/app/overview-page/overview-page.component.ts
+++ src/app/overview-page/overview-page.component.ts
@@ -15,7 +15,7 @@ export class OverviewPageComponent {
   public filter: string = '';
   public tabs: TabData[];
   public models: Model[];
-  public checked: boolean;
+  public filterUnavailable: boolean;
   public onlyBestNodes: boolean;
 
   constructor(private _dataService: DataService) {}
