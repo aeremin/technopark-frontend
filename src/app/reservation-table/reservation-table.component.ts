76ba0d1a44db19c142b1217cc0a824b972467064
Alexey Eremin <a.eremin.msu@gmail.com>
2018-08-07 22:53:36 +0200

    Fix sorting

--- src/app/reservation-table/reservation-table.component.ts
+++ src/app/reservation-table/reservation-table.component.ts
@@ -12,8 +12,8 @@ import { ReservationPasswordEnterComponent } from '../reservation-password-enter
   styleUrls: ['./reservation-table.component.css'],
   animations: [
     trigger('detailExpand', [
-      state('collapsed', style({ height: '0px', minHeight: '0', display: 'none' })),
-      state('expanded', style({ height: '*' })),
+      state('collapsed, void', style({height: '0px', minHeight: '0', display: 'none'})),
+      state('expanded', style({height: '*'})),
       transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
     ]),
   ],
